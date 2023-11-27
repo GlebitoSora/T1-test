f214526310d36406a670493a91146a70cd383e23
GlebitoSora <Gleb.stolyarov09@gmail.com>
2023-11-27 23:13:34 +0300

    changes 1

--- src/main/java/com/sora/t1_test/StringController.java
+++ src/main/java/com/sora/t1_test/StringController.java
@@ -4,17 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/test")
 public class StringController {
     @GetMapping("/get_string")
-    public Map<?,?> get_String(@RequestParam String str){
+    public Map<Character, Integer> get_String(@RequestParam String str){
         Map<Character, Integer> map = new HashMap<>();
         for (char key : str.toCharArray()) {
             if (map.containsKey(key)) {
